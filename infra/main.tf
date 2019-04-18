0029821d1b39310799d0ca1490b85df4f88c31b3
etheleon <picy2k@gmail.com>
2019-04-18 11:33:14 +0800

    Refactor kubeconfig as data, include tf backend

--- infra/main.tf
+++ infra/main.tf
@@ -52,3 +52,29 @@ resource "google_container_node_pool" "gpu_pool" {
   }
 }
 
+data "terraform_remote_state" "webservice" {
+  backend = "gcs"
+  config = {
+    bucket  = "terraform-state"
+    prefix  = "prod"
+  }
+}
+
+data "template_file" "kubeconfig" {
+  template = "${file("${path.module}/kubeconfig-template.yaml")}"
+
+  vars {
+    cluster_name    = "${google_container_cluster.gojek.name}"
+    user_name       = "${google_container_cluster.gojek.master_auth.0.username}"
+    user_password   = "${google_container_cluster.gojek.master_auth.0.password}"
+    endpoint        = "${google_container_cluster.gojek.endpoint}"
+    cluster_ca      = "${google_container_cluster.gojek.master_auth.0.cluster_ca_certificate}"
+    client_cert     = "${google_container_cluster.gojek.master_auth.0.client_certificate}"
+    client_cert_key = "${google_container_cluster.gojek.master_auth.0.client_key}"
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content  = "${data.template_file.kubeconfig.rendered}"
+  filename = "${path.module}/kubeconfig"
+}
