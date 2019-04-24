72d740edbcc03df2a40dd573c359d05d2243910d
etheleon <picy2k@gmail.com>
2019-04-24 21:32:12 +0800

    Update settings with total trips

--- components/geoapi/settings.py
+++ components/geoapi/settings.py
@@ -10,4 +10,5 @@ GCP_PROJECT = os.environ.get("GCP_PROJECT", "datascience-237903")
 ASSETS = {
     "BUCKET": os.environ.get("ASSSETS_BUCKET", "geoapi-assets"),
     "LOCAL_DIR": os.environ.get("ASSETS_DIR", "/geoapi/app/assets")
+    "TOTAL_TRIPS": os.environ.get("TOTAL_TRIPS", "total_trips.csv.gz")
 }
