0d7459aa461dcbfb027620f9e304f4eb7e95975a
etheleon <picy2k@gmail.com>
2019-04-24 21:35:14 +0800

    Fix typo

--- components/geoapi/settings.py
+++ components/geoapi/settings.py
@@ -9,6 +9,6 @@ GCP_PROJECT = os.environ.get("GCP_PROJECT", "datascience-237903")
 
 ASSETS = {
     "BUCKET": os.environ.get("ASSSETS_BUCKET", "geoapi-assets"),
-    "LOCAL_DIR": os.environ.get("ASSETS_DIR", "/geoapi/app/assets")
+    "LOCAL_DIR": os.environ.get("ASSETS_DIR", "/geoapi/app/assets"),
     "TOTAL_TRIPS": os.environ.get("TOTAL_TRIPS", "total_trips.csv.gz")
 }
